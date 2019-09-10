57f8e903ebb78c2460eba6aa7341d6dc38741849
Jan Kristian Bjerke <jan@altua.no>
2019-09-10 07:44:45 +0200

    Initial state

---
+++ DependencyMapper/DependencyMap.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DependencyMapper
+{
+    public class DependencyMap
+    {
+        public DependencyMap(ImmutableArray<Node> nodes, ImmutableArray<Edge> edges)
+        {
+            Nodes = nodes;
+            Edges = edges;
+        }
+
+        public ImmutableArray<Edge> Edges { get; }
+        public ImmutableArray<Node> Nodes { get; }
+
+        private static IEnumerable<Assembly> GetAssembliesInSameDir(Assembly assembly)
+        {
+            string path = Path.GetDirectoryName(assembly.Location);
+
+            foreach (string dll in Directory.GetFiles(path, "*.dll"))
+                yield return Assembly.LoadFrom(dll);
+        }
+
+        private static Node Load(Assembly assembly, Dictionary<string, Node> nodes, List<Edge> edges)
+        {
+            var node = new Node(assembly.GetName().Name);
+            nodes.Add(assembly.GetName().Name, node);
+
+            foreach (var dependencyName in assembly.GetReferencedAssemblies())
+            {
+                if (!nodes.TryGetValue(dependencyName.Name, out Node dependency))
+                {
+                    var dependencyAssembly = Assembly.Load(dependencyName);
+                    dependency = Load(dependencyAssembly, nodes, edges);
+                }
+
+                edges.Add(new Edge(node, dependency));
+            }
+
+            return node;
+        }
+
+        public static DependencyMap CreateFrom(Assembly assembly)
+        {
+            var ass = GetAssembliesInSameDir(assembly).ToImmutableDictionary(x => x.FullName);
+
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.AssemblyResolve += new ResolveEventHandler((o, a) =>
+            {
+                return ass[a.Name];
+            });
+
+
+
+
+
+
+
+
+            var nodes = new[]
+            {
+                new Node("System.Runtime.CompilerServices.Unsafe")
+            }.ToDictionary(x => x.Name);
+
+            var edges = new List<Edge>();
+
+
+            Load(assembly, nodes, edges);
+
+            return new DependencyMap(nodes.Values.ToImmutableArray(), edges.ToImmutableArray());
+        }
+    }
+
+    public class Edge
+    {
+        public Edge(Node from, Node to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public Node From { get; }
+        public Node To { get; }
+    }
+
+    public class Node
+    {
+        public Node(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Node node &&
+                   Name == node.Name;
+        }
+
+        public override int GetHashCode()
+        {
+            return 558414575 + EqualityComparer<string>.Default.GetHashCode(Name);
+        }
+    }
+}
---
+++ DependencyMapper/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+
+namespace DependencyMapper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var ofd = new OpenFileDialog()
+            {
+                Multiselect = false
+            };
+
+            var success = ofd.ShowDialog();
+            if (success.HasValue && success.Value)
+            {
+                var fn = ofd.FileName;
+
+                var assembly = Assembly.LoadFrom(fn);
+
+                Map = DependencyMap.CreateFrom(assembly);
+                DataContext = this;
+            }
+            else
+            {
+                Application.Current.Shutdown();
+            }
+        }
+
+        public DependencyMap Map { get; }
+
+        public Canvas VisualMap
+        {
+            get
+            {
+
+                var canvas = new Canvas();
+
+                canvas.Children.Add(new Button() { Content = "Test" });
+
+                return canvas;
+            }
+        }
+
+    }
+}
